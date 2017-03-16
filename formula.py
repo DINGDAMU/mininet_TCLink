57bfc18e6f0677aefad8a1154b9a3242614f759a
dingdamu <dingdamu@gmail.com>
2017-03-16 17:50:49 +0100

    indent python

--- formula.py
+++ formula.py
@@ -1,81 +1,90 @@
-#!/usr/bin/python  
-#-*- coding:utf-8 -*-  
-############################  
-#File Name: formula1.py
-#Author: dingdamu  
-#Mail: dingdamu@gmail.com  
-#Created Time: 2017-02-16 12:47:49
-############################  
+# -*- coding:utf-8 -*-
+############################
+# File Name: formula1.py
+# Author: dingdamu
+# Mail: dingdamu@gmail.com
+# Created Time: 2017-02-16 12:47:49
+############################
 
 from __future__ import division
 import math
 import json
-import os
 
-#d is the separating distance between transmitter-receiver pair(m)
+# d is the sep#!/usr/bin/python
 pl_model = '28'
+# d = 10    #the separating distance between transmitter-receiver pair(m)
 if cmp(pl_model, '28') == 0:
-    alpha = 72.0 #dB
+    alpha = 72.0   # dB
     beta = 2.92
-    sigma = 8.7 #dB
+    sigma = 8.7    # dB
     sigma_lin = 10**(sigma/10)
     xsi_lin = sigma_lin**2
     xsi = 10*math.log10(xsi_lin)
+
     def pl(d):
-        return  alpha + 10* beta * math.log10(d)
+        return alpha + 10 * beta * math.log10(d)
 elif cmp(pl_model, '73') == 0:
-    alpha = 86.6 #dB
+    alpha = 86.6   # dB
     beta = 2.45
-    sigma = 8.0 #dB
+    sigma = 8.0   # dB
     sigma_lin = 10**(sigma/10)
     xsi_lin = sigma_lin**2
     xsi = 10*math.log10(xsi_lin)
+
     def pl(d):
-        return  alpha + 10* beta * math.log10(d)
+        return alpha + 10 * beta * math.log10(d)
 elif cmp(pl_model, '3gpp') == 0:
     fc = 2.5
+
     def pl(d):
         return 22.7 + 36.7*math.log10(d) + 26*math.log10(fc)
     beta = 3.67
-    alpha=70.0 #22.7 + 26*log10(fc)
+    alpha = 70.0    # 22.7 + 26*log10(fc)
+
+lamda = 100.0    # density of mm-Wave links
+B = 2e9         # mmWave bandwidth
+C = 0.11        # fractional LoS area in the model developed
 
 
-lamda =100.0 #density of mm-Wave links
-B = 2e9 #mmWave bandwidth
-C = 0.11 #fractional LoS area in the model developed
 def D(d):
     return d
-Gmax = 18.0 #db
+
+
+Gmax = 18.0    # db
 Gmax_lin = 10**(Gmax/10)
 
-Pb = 30.0 #dBm
+Pb = 30.0    # dBm
 Pb_lin = 10**(Pb/10)
 
-pn = -174.0 + 10*(math.log10(B)) + 10 # noise power
+pn = -174.0 + 10*(math.log10(B)) + 10       # noise power
 pn_lin = 10 ** (pn/10)
 
 
 def pl_lin(d):
-    return  10** (pl(d)/10)
+    return 10 ** (pl(d)/10)
 
-#the unit of pl is dB?
-SNR0 = Pb + Gmax -pn
+# the unit of pl is dB?
+SNR0 = Pb + Gmax - pn
 SNR_lin = 10 ** (SNR0/10)
+
+
 def SNR(d):
-    return  SNR0 - pl(d)
+    return SNR0 - pl(d)
+
+
 def SNR_lin(d):
-    return  10**(SNR(d)/10)
+    return 10**(SNR(d)/10)
 
 
-#xsi corresponding path-loss standard deviation
-xsi_l_lin=5.2
-#xsi_l_lin = 10^(xsi_l/10)
+# xsi corresponding path-loss standard deviation
+xsi_l_lin = 5.2
+# xsi_l_lin = 10^(xsi_l/10)
 xsi_n_lin = 7.6
-#xsi_n_lin = 10^(xsi_n/10)
+# xsi_n_lin = 10^(xsi_n/10)
 
 
-#beta_l_n = one_meter_loss #dB
-beta_l_n = alpha #dB
+# beta_l_n = one_meter_loss #dB
+beta_l_n = alpha    # dB
 beta_l_n_lin = 10**(beta_l_n/10)
 
 # beta_l_lin = beta_l_n_lin
@@ -83,75 +92,90 @@ beta_l_n_lin = 10**(beta_l_n/10)
 
 # ml_db = -0.1*beta_l_lin * log(10)
 ml = -math.log(beta_l_n_lin)  # 10^(ml_db/10)
-sigma_l = 0.1* xsi_l_lin * math.log(10)
+sigma_l = 0.1 * xsi_l_lin * math.log(10)
 
 # mn_db = -0.1*beta_n_lin * log(10);
 mn = -math.log(beta_l_n_lin)  # 10^(mn_db/10)
-sigma_n = 0.1* xsi_n_lin * math.log(10)
+sigma_n = 0.1 * xsi_n_lin * math.log(10)
 
 tau = 3.0  # pl(i)
 tau_lin = 10**(tau/10)
 
+
 def q_fun(x):
     return 0.5*math.erfc(x/math.sqrt(2))
 
+
 def factor(d):
-    return SNR_lin(d)/tau_lin #((Pb_lin*Gmax_lin)/(pl_lin*pn_lin))
+    return SNR_lin(d)/tau_lin   # ((Pb_lin*Gmax_lin)/(pl_lin*pn_lin))
 
 
 def pc1_q1(d):
     return q_fun((math.log(D(d)**beta/factor(d))-ml)/sigma_l)
 
+
 def pc1_q2(d):
     return q_fun((math.log(D(d)**pl(d)/factor(d))-mn)/sigma_n)
+
 
 def pc1(d):
     return D(d)**2*(pc1_q1(d)-pc1_q2(d))
 
-def pc_c1(sigma,m,d):
+
+def pc_c1(sigma, m, d):
     return factor(d)**(2/beta)*math.exp(2*(sigma**2/beta**2)+2*(m/beta))
 
-def pc_c2(sigma,m,d):
+
+def pc_c2(sigma, m, d):
     return q_fun((sigma**2*(2/beta)-math.log(D(d)**beta/factor(d))+m)/sigma)
 
+
 def pc2(d):
-     return pc_c1(sigma_l,ml,d)*pc_c2(sigma_l,ml,d)
+    return pc_c1(sigma_l, ml, d)*pc_c2(sigma_l, ml, d)
+
 
 def pc3(d):
-    return pc_c1(sigma_n,mn,d)*(1/C-pc_c2(sigma_n,mn,d))
+    return pc_c1(sigma_n, mn, d)*(1/C-pc_c2(sigma_n, mn, d))
+
 
 def pc(d):
     return pc1(d)+pc2(d)+pc3(d)
 
+
 def Lamda_a(d):
     return lamda*math.pi*C*pc(d)
+
 
 def Ma(d):
     return Lamda_a(d)/lamda
 
+
 def ps(d):
     return 1-math.exp(-lamda*Ma(d)*factor(d))
 
+
 def p_loss(d):
     return (1-float(ps(d)))*100
+
+
 annotation = {
-            "apps":{
-                "org.onosproject.millimeterwavelink":{
-                    "links":[{
-                        "src":"of:000000000000000e/5",
-                        "dst":"of:000000000000000f/3",
+            "apps": {
+                "org.onosproject.millimeterwavelink": {
+                    "links": [{
+                        "src": "of:000000000000000e/5",
+                        "dst": "of:000000000000000f/3",
                         "length": "100",
-                        "capacity":"200",
-                        "technology":"mmwave",
+                        "capacity": "200",
+                        "technology": "mmwave",
                         "ps": 100 - p_loss(10)
                         }]
                     },
-                "org.onosproject.millimeterwaveport":{
-                    "ports":[{
-                        "technology":"mmwave",
+                "org.onosproject.millimeterwaveport": {
+                    "ports": [{
+                        "technology": "mmwave",
                         "deviceID": "of:000000000000000a",
-                        "portnumber":"1",
-                        "isEnabled":"true"
+                        "portnumber": "1",
+                        "isEnabled": "true"
                         }]
                     }
                 },
@@ -177,7 +201,7 @@ annotation = {
             "hosts": {
                 "00:00:00:00:00:01/None": {
                     "basic": {
-                        "name":"host1"
+                        "name": "host1"
                         }
                     }
                 },
@@ -185,24 +209,25 @@ annotation = {
                 "of:000000000000000a/1": {
                     "interfaces": [
                         {
-                            "name":"port1"
+                            "name": "port1"
                             }
                         ]
-                    },
+                },
                 "of:000000000000000b/2": {
                     "interfaces": [
                         {
-                            "name":"port2"
+                            "name": "port2"
                             }
                         ]
                     }
                 }
             }
+
+
 def onosjson():
-    fp = open("cfg.json","w")
-    fp.write(json.dumps(annotation,sort_keys=True,indent = 2))
+    fp = open("cfg2.json", "w")
+    fp.write(json.dumps(annotation, sort_keys=True, indent=2))
     fp.close()
-    os.system("onos-netcfg localhost cfg.json")
 
 if __name__ == '__main__':
     onosjson()
